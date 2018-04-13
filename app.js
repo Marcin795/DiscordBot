6df0349400c815125addaf8fd1610f13d8e0e2b9
Marcin795 <38189523+Marcin795@users.noreply.github.com>
2018-04-13 16:03:18 +0200

    SQLite v0.1

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const token = require('./settings.json').token;
 const http = require('http');
 const https = require('https');
 const fs = require('fs');
+const sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('./db/plan.db', (err) =>{
+  if (err) {
+    console.error(err.message);
+  } else console.log('Henlo from database.');
+});
 var emotki;
-var wejsciowki;
-var kolokwia;
-var egzaminy;
-var sprawozdania;
 
 client.on('ready', () => {
   console.log('Henlo I works');
